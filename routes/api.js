5d5ec8bb9204b00a4b61f9a265b66c27ff0ccc91
tyagi <tyagi@PC1-11-0022.local>
2013-03-15 13:09:48 +0900

    update component.json, package.json

---
+++ routes/api.js
@@ -0,0 +1,50 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: tyagi
+ * Date: 13/03/15
+ * Time: 12:44
+ * To change this template use File | Settings | File Templates.
+ */
+
+//feeds
+/**
+ *
+ * page_id: ページング
+ * item_filter: 未読, 全て
+ * category: カテゴリ
+ * tags: タグ
+ * word_filter: 絞り込み
+ * sort: 新しい、古い
+ *
+ */
+exports.feeds = function(req, res){
+    res.send("respond with a resource");
+};
+
+//登録フィードを取得する
+exports.feed_get_subscriptions = function(req, res){
+    res.send("respond with a resource");
+};
+
+//フィードを追加
+exports.feed_add_subscribe = function(req, res){
+
+};
+//フィードを削除
+exports.feed_remove_subscribe = function(req, res){
+    res.send("respond with a resource");
+};
+
+
+//カテゴリの追加
+//カテゴリの取得
+//カテゴリの削除
+
+//タグの追加
+//タグの取得
+//タグの削除
+
+
+
+
+
