67083b3abd2e276e59be671cfb56806345dc3f89
yagi <sylc.yagi@gmail.com>
2013-04-01 01:07:00 +0900

    設定ファイルを追加

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,32 @@
-
 /*
  * GET home page.
  */
 
-exports.index = function(req, res){
-  console.log(req.session);
-  req.session.oauth = "none";
-
-  res.render('index', { title: 'GoatReader -Home-' });
+exports.index = function (req, res) {
+    res.render('index', { title: 'GoatReader -Home-' });
+    /*
+    if (!req.session.isLogin) {
+        res.render('login', { title: 'GoatReader -Login-' });
+    }
+    else {
+        res.render('index', { title: 'GoatReader -Home-' });
+    }
+    */
 };
-exports.about = function(req, res){
-    console.log(req.session);
+exports.about = function (req, res) {
     res.render('about', { title: 'GoatReader -About-' });
+
 };
-exports.settings = function(req, res){
-    console.log(req.session);
+exports.settings = function (req, res) {
     res.render('settings', { title: 'GotReader -Settings-' });
+    /*
+    if (!req.session.isLogin) {
+        res.render('login', { title: 'GoatReader -Login-' });
+    }
+    else {
+        res.render('settings', { title: 'GotReader -Settings-' });
+    }
+    */
 };
 
 
---
+++ settings.js
@@ -0,0 +1,17 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: yagitoshihiro
+ * Date: 2013/04/01
+ * Time: 0:46
+ * To change this template use File | Settings | File Templates.
+ */
+exports.settings = {
+
+    //mode: anonymouse | twitter
+    mode:"anonymouse"
+    ,twitter:{
+        twitterConsumerKey:"set your twitter app consumer key."
+        ,twitterConsumerSecret:"set your twitter app consumer secret."
+        ,twitterAuthCallbackURL:"set your oauth callback url."
+    }
+};
