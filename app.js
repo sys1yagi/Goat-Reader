7a17e21bab94614e76df8aebea02d0d5eb78c7c6
tyagi <tyagi@PC1-11-0022.local>
2013-04-03 17:41:33 +0900

    add favicon.

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.configure(function () {
     app.set('port', process.env.PORT || local_port);
     app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
-    app.use(express.favicon());
+    //app.use(express.favicon());
     app.use(express.logger('dev'));
     app.use(express.bodyParser());
     app.use(express.methodOverride());
