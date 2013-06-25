890b2b4c37c11c4bf6b32d9f72c22f7cd61b6747
yagi <sylc.yagi@gmail.com>
2013-06-26 03:02:14 +0900

    rename.

--- settings.js
+++ settings.js
@@ -4,7 +4,6 @@ exports.auth = {
     twitter:{
         twitterConsumerKey:"set your twitter app consumer key."
         ,twitterConsumerSecret:"set your twitter app consumer secret."
-        ,twitterAuthCallbackURL:"set your oauth callback url."
         ,callbackURL:"set your callback url."
     }
 };
