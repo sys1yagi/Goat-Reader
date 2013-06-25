27a3e4d9688ea0454e0e63171e512605d17bb8a1
yagi <sylc.yagi@gmail.com>
2013-06-26 02:31:30 +0900

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,20 +1,11 @@
-/**
- * Created with JetBrains WebStorm.
- * User: yagitoshihiro
- * Date: 2013/04/01
- * Time: 0:46
- * To change this template use File | Settings | File Templates.
- */
 exports.local_port=3001;
 exports.debug = false;
-//boot mode
 exports.auth = {
     twitter:{
         twitterConsumerKey:"set your twitter app consumer key."
         ,twitterConsumerSecret:"set your twitter app consumer secret."
         ,twitterAuthCallbackURL:"set your oauth callback url."
-        ,twitterDomain:"set your domain."
+        ,callbackURL:"set your callback url."
     }
 };
-
 exports.cronTime = "*/30 * * * *";
